4f192260262160c769c0a13594fbebe4e50270af
Koh Nakagawa <tsunekou1019@gmail.com>
2021-02-13 22:30:29 +0900

    initial commit

---
+++ CalcCyclomaticForAllFunctions.py
@@ -0,0 +1,32 @@
+#TODO write a description for this script
+#@author 
+#@category CodeAnalysis
+#@keybinding 
+#@menupath 
+#@toolbar 
+
+
+from __main__ import *
+from ghidra.program.util import CyclomaticComplexity
+
+
+def get_complex_functions_list(num):
+    functions = currentProgram.getFunctionManager().getFunctions(True)
+
+    func_to_complexity = list()
+    cc = CyclomaticComplexity()
+    for function in functions:
+        complexity = cc.calculateCyclomaticComplexity(function, monitor)
+        func_to_complexity.append({
+            "function": str(function),
+            "address": function.getEntryPoint(),
+            "complexity": complexity
+        })
+
+    func_to_complexity_sorted = sorted(func_to_complexity, key=lambda elem: elem["complexity"], reverse=True)
+    return func_to_complexity_sorted[:num]
+
+
+
+if __name__ == "__main__":
+    print(get_complex_functions_list(10))
---
+++ README.md
@@ -0,0 +1,19 @@
+# My Ghidra scripts 
+
+This repository contains some Ghidra scripts to automate my reverse-engineering task.
+
+## Descriptions
+
+****
+CalcCyclomaticForAllFunctions.java   hash_db.json   ModifyDecompileResultFromApiHash.java        README.md
+ CalcCyclomaticForAllFunctions..py  'FindParentFunctions$py.class'        LICENSE        ModifyDecompileResultFromFunctionName.java   ShowFunctionCallTrees.java
+
+****
+
+## Author
+
+Koh M. Nakagawa
+
+## License
+
+MIT License
